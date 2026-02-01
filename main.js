eac88a73d5f4f8fe7581cae9fe284b170f127c53
Azraeth <allan.almoguera20@gmail.com>
2026-02-01 22:06:15 +0800

    Finalized hero section

---
+++ main.js
@@ -0,0 +1,13 @@
+// Initialize the Typed.js animation for the "I am a Web Developer" part
+var typed = new Typed('.text', {
+  strings: [
+    'a Web Developer',
+    'an Application Developer',
+    'a Front-End Developer',
+    'a Back-End Developer',
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 500,
+  loop: true,
+});
